cb4a7e8ebca686a58e3d89417073a68dd000f33a
diego <diegoortega1606@gmail.com>
2025-05-10 12:48:23 +0200

    prueba lint con error

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ export const App = () => {
 
       let regStart, regEnd;
       if (reg === "kanto") {
-        ssressagStart = 0;
+        ssressaagStart = 0;
         regEnd = 151;
       } else if (reg === "johto") {
         regStart = 151;
