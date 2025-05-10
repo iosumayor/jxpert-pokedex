15245956bed6da0ae2066c5b021789a78432ef0b
diego <diegoortega1606@gmail.com>
2025-05-10 13:34:37 +0200

    error al subir

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ export const App = () => {
 
       let regStart, regEnd;
       if (reg === "kanto") {
-        sssressassssgStart = 0;
+        sssresssassssgStart = 0;
         regEnd = 151;
       } else if (reg === "johto") {
         regStart = 151;
