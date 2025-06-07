0ab4d3f7ff061e7402757241bf6edf84e37f8ecf
iosumayor <iosumayor23@gmail.com>
2025-06-07 09:39:36 +0200

    refactor: llevamos las regiones al archivo constants

--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -1,48 +1,4 @@
-import { Region, REGIONS } from "../constants/region";
-
-type RegionRangeItem = {
-  start: number;
-  end: number;
-};
-
-const regionRanges: Record<Region, RegionRangeItem> = {
-  kanto: {
-    start: 0,
-    end: 151,
-  },
-  johto: {
-    start: 151,
-    end: 251,
-  },
-  hoenn: {
-    start: 251,
-    end: 386,
-  },
-  sinnoh: {
-    start: 386,
-    end: 494,
-  },
-  unova: {
-    start: 494,
-    end: 649,
-  },
-  kalos: {
-    start: 649,
-    end: 721,
-  },
-  alola: {
-    start: 721,
-    end: 809,
-  },
-  galar: {
-    start: 809,
-    end: 905,
-  },
-  paldea: {
-    start: 905,
-    end: 1025,
-  },
-} as const;
+import { Region, REGIONS, regionRanges } from "../constants/region";
 
 export const usePokemons = () => {
   const getCurrentRegion = (region: Region) => {
